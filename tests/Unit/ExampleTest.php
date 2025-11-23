ada1cc59d927b8632b0d75da29faf89a8f7895cd
Douaa <douaa.kaddar@insbaixcamp.car>
2025-11-23 20:10:56 +0100

    evitar error directorio vacío

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * Test básico de ejemplo para Unit tests.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
